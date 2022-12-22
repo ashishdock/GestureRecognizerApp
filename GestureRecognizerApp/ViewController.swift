c7d11c0a683ac167da96155da93e2766d1069d9e
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-22 13:14:03 +0530

    Completed. Includes Tap gesture, not pinch.

--- GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/ViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImageAndLabel))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func changeImageAndLabel() {
+        print("tapped")
+        if imageView.image == UIImage(named: "Wallpaper") {
+            imageView.image = UIImage(named: "Apple-logo-wallpaper-hd-backgrounds")
+            myLabel.text = "Apple Logo Image"
+        } else {
+            imageView.image = UIImage(named: "Wallpaper")
+            myLabel.text = "Nature Image"
+        }
+
+
     }
 
 
